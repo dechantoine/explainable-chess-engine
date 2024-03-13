a3adfed4821ea8f848d5bba8c1f7c6aa11fb4ba8
Antoine Déchappe <dechantoine@gmail.com>
2024-03-13 16:57:41 +0100

    feat: getitem & getitems

--- dataset.py
+++ dataset.py
@@ -6,19 +6,22 @@ import torch
 from torch import Tensor
 from torch.utils.data import Dataset
 
-from utils import format_board
+from utils import board_to_tensor, moves_to_tensor, batch_boards_to_tensor, batch_moves_to_tensor
 from loguru import logger
 
 
 class ChessBoardDataset(Dataset):
     """Chess boards with legal moves dataset."""
 
-    def __init__(self, root_dir):
+    @logger.catch
+    def __init__(self, root_dir, transform=None, target_transform=None):
         """
         Arguments:
             root_dir (string): Directory with all the PGNs.
         """
         self.root_dir = root_dir
+        self.transform = transform
+        self.target_transform = target_transform
         self.list_pgn_files = [f for f in os.listdir(self.root_dir) if f.endswith(".pgn")]
         self.board_indices = self.get_boards_indices()
 
@@ -36,34 +39,60 @@ class ChessBoardDataset(Dataset):
                 j += 1
         return list_board_indices
 
+
+    @logger.catch
+    def retrieve_board(self, idx: int):
+        file_id, game_id, move_id = self.board_indices[idx]
+        file = self.list_pgn_files[file_id]
+        logger.info(f"file: {file}, game_id: {game_id}, move_id: {move_id}")
+        pgn = open(self.root_dir + "/" + file)
+
+        game = chess.pgn.read_game(pgn)
+        for j in range(game_id):
+            game = chess.pgn.read_game(pgn)
+
+        board = game.board()
+        for move in list(game.mainline_moves())[:move_id]:
+            board.push(move)
+        return board
+
+    @logger.catch
     def __len__(self):
         return len(self.board_indices)
 
-    def __getitem__(self, indices: Union[Tensor, list[int]]):
+    @logger.catch
+    def __getitem__(self, idx: Union[Tensor, int]):
+        if torch.is_tensor(idx):
+            idx = int(idx.item())
+
+        board_sample = self.retrieve_board(idx)
+        legal_moves_sample = list(board_sample.legal_moves)
+
+        if self.transform:
+            board_sample = torch.tensor(board_to_tensor(board_sample))
+
+        if self.target_transform:
+            legal_moves_sample = torch.tensor(moves_to_tensor(legal_moves_sample))
+
+        return board_sample, legal_moves_sample
+
+    @logger.catch
+    def __getitems__(self, indices: Union[Tensor, list[int]]):
         if torch.is_tensor(indices):
             indices = indices.tolist()
 
-        sample = dict()
+        board_samples = []
+        legal_moves_samples = []
 
         for i in indices:
+            board = self.retrieve_board(i)
+            board_samples.append(board)
+            legal_moves_samples.append(list(board.legal_moves))
 
-            file_id, game_id, move_id = self.board_indices[i]
-            file = self.list_pgn_files[file_id]
-            logger.info(f"file: {file}, game_id: {game_id}, move_id: {move_id}")
-            pgn = open(self.root_dir + "/" + file)
+        if self.transform:
+            board_samples = torch.tensor(batch_boards_to_tensor(board_samples))
 
-            game = chess.pgn.read_game(pgn)
-            for j in range(game_id):
-                game = chess.pgn.read_game(pgn)
+        if self.target_transform:
+            legal_moves_samples = torch.tensor(batch_moves_to_tensor(legal_moves_samples))
 
-            board = game.board()
-            for move in list(game.mainline_moves())[:move_id]:
-                board.push(move)
-
-            sample[i] = {"board": format_board(board),
-                         "legal_moves": list(board.legal_moves)}
-
-        #if self.transform:
-        #    sample = self.transform(sample)
-
-        return sample
+        return board_samples, legal_moves_samples
