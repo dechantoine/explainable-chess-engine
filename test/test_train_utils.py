c2b1fc4838384f12245eca1e2c1dbf45115f4f55
Antoine Déchappe <dechantoine@gmail.com>
2024-03-24 18:07:42 +0100

    test: train test split

---
+++ test/test_train_utils.py
@@ -0,0 +1,27 @@
+from src.data.dataset import ChessBoardDataset
+from src.train.train_utils import train_test_split
+
+from loguru import logger
+
+import unittest
+
+
+class TrainUtilsTestCase(unittest.TestCase):
+    def setUp(self):
+        self.dataset = ChessBoardDataset(root_dir="../test/test_data",
+                                         return_moves=False,
+                                         return_outcome=False,
+                                         transform=False)
+
+    @logger.catch
+    def test_train_test_split(self):
+        train_set, test_set = train_test_split(self.dataset, seed=42, train_size=0.8)
+
+        assert train_set.list_pgn_files == test_set.list_pgn_files
+        assert isinstance(train_set, ChessBoardDataset)
+        assert isinstance(test_set, ChessBoardDataset)
+        assert len(train_set) + len(test_set) == len(self.dataset)
+        assert all(i not in test_set.board_indices for i in train_set.board_indices)
+        assert all(i not in train_set.board_indices for i in test_set.board_indices)
+        assert all(i in self.dataset.board_indices for i in train_set.board_indices)
+        assert all(i in self.dataset.board_indices for i in test_set.board_indices)
