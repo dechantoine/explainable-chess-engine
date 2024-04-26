d34564cc833f7ecc8cbb2b0be6a9067a30334a4a
Antoine Déchappe <dechantoine@gmail.com>
2024-04-26 18:27:10 +0200

    perf: profile dataset init

--- profile/profile_dataset.py
+++ profile/profile_dataset.py
@@ -2,33 +2,32 @@ import os
 from cProfile import Profile
 from pstats import SortKey, Stats
 
-from loguru import logger
-
 from src.data.dataset import ChessBoardDataset
 
 
 class ChessBoardProfiling:
     def __init__(self):
+        self.dataset = None
+        self.n_test = 100
+
+    def init(self):
         self.dataset = ChessBoardDataset(
             root_dir="../test/test_data",
             return_moves=True,
             return_outcome=True,
             transform=True,
             include_draws=False,
+            in_memory=True,
         )
-        self.n_test = 100
 
-    @logger.catch
     def get_boards_indices(self):
         for i in range(self.n_test):
             _ = self.dataset.get_boards_indices(include_draws=True)
 
-    @logger.catch
     def retrieve_board(self):
         for i in range(self.n_test):
             _, _, _, _ = self.dataset.retrieve_board(0)
 
-    @logger.catch
     def getitems(self):
         for i in range(self.n_test):
             _, _, _ = self.dataset.__getitems__(list(range(64)))
@@ -38,6 +37,14 @@ if __name__ == "__main__":
     chess_profiling = ChessBoardProfiling()
 
     os.makedirs("./dataset", exist_ok=True)
+
+    with Profile() as profiler:
+        profiler.runcall(chess_profiling.init)
+
+        stats = Stats(profiler)
+        stats.strip_dirs()
+        stats.sort_stats(SortKey.TIME)
+        stats.dump_stats("./dataset/init.prof")
 
     with Profile() as profiler:
         profiler.runcall(chess_profiling.get_boards_indices)
