e8cbc2d94da1ef1ec66726ebf21e81cc18df3538
Antoine Déchappe <dechantoine@gmail.com>
2024-05-18 10:18:19 +0200

    refactor: config dict for training

--- src/train/train.py
+++ src/train/train.py
@@ -5,7 +5,20 @@ from torch.utils.data import DataLoader
 
 from src.data.dataset import ChessBoardDataset
 from src.models.simple_feed_forward import SimpleFF
-from src.train.train_utils import train_test_split, training_loop
+from src.train.train_utils import init_training, train_test_split, training_loop
+
+train_params = {
+    "run_name": "simple_ff_0",
+    "dataset_num_workers": 8,
+    "dataloaders_num_workers": 2,
+    "train_size": 0.90,
+    "n_epochs": 8,
+    "batch_size": 64,
+    "lr": 0.05,
+    "gamma": 0.99,
+    "log_sampling": 0.05,
+    "eval_sampling": 1,
+}
 
 
 def collate_fn(x):
@@ -19,6 +32,22 @@ def collate_fn(x):
 
 
 if __name__ == "__main__":
+    logger.info("Initializing model, optimizer, and loss.")
+    model = SimpleFF()
+    optimizer = Adagrad(
+        model.parameters(),
+        lr=train_params["lr"],
+    )
+    loss = MSELoss()
+
+    model, optimizer, resume_step = init_training(
+        train_params["run_name"], model, optimizer
+    )
+
+    logger.info(
+        f"Model number of trainable parameters: {sum(p.numel() for p in model.parameters() if p.requires_grad)}"
+    )
+
     logger.info("Loading data.")
     dataset = ChessBoardDataset(
         root_dir="./sample_data",
@@ -27,54 +56,46 @@ if __name__ == "__main__":
         return_outcome=True,
         include_draws=False,
         in_memory=True,
-        num_workers=8,
+        num_workers=train_params["dataset_num_workers"],
     )
     logger.info(f"Dataset size: {len(dataset)}")
 
     logger.info("Splitting data.")
     train_dataset, test_dataset = train_test_split(
-        dataset=dataset, seed=0, train_size=0.90, stratify=True
+        dataset=dataset, seed=0, train_size=train_params["train_size"], stratify=True
     )
 
     logger.info(f"Train dataset size: {len(train_dataset)}")
     logger.info(f"Test dataset size: {len(test_dataset)}")
 
     train_dataloader = DataLoader(
-        train_dataset, batch_size=64, shuffle=True, collate_fn=collate_fn, num_workers=2
+        train_dataset,
+        batch_size=train_params["batch_size"],
+        shuffle=True,
+        collate_fn=collate_fn,
+        num_workers=train_params["dataloaders_num_workers"],
     )
 
     test_dataloader = DataLoader(
-        test_dataset, batch_size=64, shuffle=True, collate_fn=collate_fn, num_workers=2
+        test_dataset,
+        batch_size=train_params["batch_size"],
+        shuffle=True,
+        collate_fn=collate_fn,
+        num_workers=train_params["dataloaders_num_workers"],
     )
-
-    logger.info("Initializing model, optimizer, and loss.")
-    model = SimpleFF()
-    optimizer = Adagrad(
-        model.parameters(),
-        lr=0.05,
-        lr_decay=0.001,
-    )
-    loss = MSELoss()
-    logger.info(
-        f"Model number of trainable parameters: {sum(p.numel() for p in model.parameters() if p.requires_grad)}"
-    )
-
-    # with profile(activities=[ProfilerActivity.CPU],
-    #             record_shapes=True) as prof:
 
     logger.info("Starting training loop.")
     training_loop(
         model=model,
         optimizer=optimizer,
         loss=loss,
-        gamma=0.99,
+        gamma=train_params["gamma"],
         train_dataloader=train_dataloader,
         test_dataloader=test_dataloader,
-        n_epochs=10,
+        n_epochs=train_params["n_epochs"],
+        resume_step=resume_step,
         device="cpu",
-        log_sampling=0.05,
-        eval_sampling=1,
-        run_name="simple_ff_X",
+        log_sampling=train_params["log_sampling"],
+        eval_sampling=train_params["eval_sampling"],
+        run_name=train_params["run_name"],
     )
-
-    # logger.info(prof.key_averages(group_by_input_shape=True).table(sort_by="cpu_time_total", row_limit=10))
