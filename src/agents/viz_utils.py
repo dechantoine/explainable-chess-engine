ec34d73874aca37da2c199be0e38b0b167777e16
Antoine Déchappe <dechantoine@gmail.com>
2024-06-16 21:34:49 +0200

    feat: cairo svg to png for HF space

--- src/agents/viz_utils.py
+++ src/agents/viz_utils.py
@@ -1,13 +1,16 @@
 import os
 
+import cairosvg
 import chess.svg
 import graphviz
 from anytree import AnyNode
+from loguru import logger
 
 graph_params = {
     "name": "Beam Search",
     "format": "png",
     "shape": "none",
+    "board_size": 300,
     "fontsize": "20",
     "image_pos": "tc",
     "labelloc": "b",
@@ -17,6 +20,7 @@ graph_params = {
 }
 
 
+@logger.catch(level="DEBUG")
 def board_to_svg(board: chess.Board, is_pruned: bool = False, size: int = 360) -> str:
     """Convert a chess.Board object to an SVG string.
 
@@ -45,22 +49,36 @@ def board_to_svg(board: chess.Board, is_pruned: bool = False, size: int = 360) -
     return svg
 
 
-def save_svg(board: chess.Board, filename: str, is_pruned: bool = False):
-    """Save a chess.Board object to an SVG file.
+@logger.catch(level="DEBUG")
+def save_svg(
+    board: chess.Board, filename: str, is_pruned: bool = False, to_png=False
+) -> None:
+    """Save a chess.Board object to an image file.
 
     Args:
         board (chess.Board): chess.Board object
         filename (str): filename to save the SVG file
         is_pruned (bool): whether the board is pruned or not
+        to_png (bool): whether to save the SVG file as a PNG file
 
     """
     xml_declaration = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>'
-    with open(filename, "w") as f:
-        f.write(xml_declaration + board_to_svg(board, is_pruned=is_pruned))
+    with open(filename + ".svg", "w") as f:
+        f.write(
+            xml_declaration
+            + board_to_svg(board, is_pruned=is_pruned, size=graph_params["board_size"])
+        )
+
+    if to_png:
+        cairosvg.svg2png(url=filename + ".svg", write_to=filename + ".png")
 
 
+@logger.catch(level="DEBUG", reraise=True)
 def plot_save_beam_search(
-    beam: AnyNode, filename: str, temp_dir: str = "../reports/figures"
+    beam: AnyNode,
+    filename: str,
+    temp_dir: str = "../reports/figures",
+    intermediate_png: bool = False,
 ) -> None:
     """Plot and save the beam search tree.
 
@@ -68,11 +86,17 @@ def plot_save_beam_search(
         beam (AnyNode): beam search tree
         filename (str): filename to save the tree plot
         temp_dir (str): temporary directory to save the SVG files
+        intermediate_png (bool): whether to save the SVG files as PNG files
 
     """
-    save_svg(board=beam.board, filename=os.path.join(temp_dir, "root.svg"))
+    image_path = os.path.abspath(os.path.join(temp_dir, "root"))
+    logger.info(image_path)
+    save_svg(board=beam.board, filename=image_path, to_png=intermediate_png)
 
     dot = graphviz.Digraph(name=graph_params["name"], format=graph_params["format"])
+
+    image_path = image_path + ".png" if intermediate_png else image_path + ".svg"
+    logger.info(image_path)
     dot.node(
         name="ROOT",
         label="",
@@ -80,7 +104,7 @@ def plot_save_beam_search(
         fontsize=graph_params["fontsize"],
         shape=graph_params["shape"],
         image_pos=graph_params["image_pos"],
-        image=os.path.join(temp_dir, "root.svg"),
+        image=image_path,
     )
 
     for board in beam.descendants:
@@ -89,12 +113,18 @@ def plot_save_beam_search(
         is_pruned = not (
             any(board.depth == beam.height for board in board.descendants)
         ) and not (board.depth == beam.height)
+
+        image_path = os.path.abspath(os.path.join(temp_dir, board.name))
+        logger.info(image_path)
         save_svg(
             board=board.board,
-            filename=os.path.join(temp_dir, f"{board.name}.svg"),
+            filename=image_path,
             is_pruned=is_pruned,
+            to_png=intermediate_png,
         )
 
+        image_path = image_path + ".png" if intermediate_png else image_path + ".svg"
+        logger.info(image_path)
         dot.node(
             name=board.name,
             label="score = {:.4f}".format(board.score),
@@ -102,7 +132,7 @@ def plot_save_beam_search(
             fontsize=graph_params["fontsize"],
             shape=graph_params["shape"],
             image_pos=graph_params["image_pos"],
-            image=os.path.join(temp_dir, f"{board.name}.svg"),
+            image=image_path,
         )
 
         dot.edge(
@@ -114,9 +144,21 @@ def plot_save_beam_search(
             tailport=graph_params["tailport"],
         )
 
-    dot.render(filename=filename, format="png", cleanup=True)
+    dot.render(
+        filename=filename,
+        format=graph_params["format"],
+        # renderer="cairo",
+        # formatter="cairo",
+    )
+
+    logger.info(os.listdir(temp_dir))
+
+    # log render file
+    with open(filename, "r") as f:
+        logger.info(f.read())
 
     # delete all svg files
-    for file in os.listdir(temp_dir):
-        if file.endswith(".svg"):
-            os.remove(os.path.join(temp_dir, file))
+    # for file in os.listdir(temp_dir):
+    #    if file.endswith(".svg"):
+    #        logger.info("Deleting : " + os.path.join(temp_dir, file))
+    #        os.remove(os.path.join(temp_dir, file))
