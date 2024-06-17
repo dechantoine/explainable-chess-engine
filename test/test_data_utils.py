50c84b1d1bcc9a09769ffc7c561c191afa416c37
Antoine Déchappe <dechantoine@gmail.com>
2024-06-17 16:07:04 +0200

    test: read all boards from a pgn file

--- test/test_data_utils.py
+++ test/test_data_utils.py
@@ -7,6 +7,7 @@ from src.data.data_utils import (
     board_to_tensor,
     format_board,
     moves_to_tensor,
+    read_boards_from_pgn,
     result_to_tensor,
     string_to_array,
     uci_to_coordinates,
@@ -14,9 +15,14 @@ from src.data.data_utils import (
 
 
 class DataUtilsTestCase(unittest.TestCase):
+    def setUp(self):
+        self.pgn_path = "test/test_data/Najdorf.pgn"
+        self.len_moves_najdorf = [73, 58, 44, 87]
+
     def test_format_board(self):
         board = chess.Board()
         board = format_board(board)
+
         assert isinstance(board, str)
         assert len(board) == 64
 
@@ -24,12 +30,14 @@ class DataUtilsTestCase(unittest.TestCase):
         board = chess.Board()
         board = format_board(board)
         board = string_to_array(board)
+
         assert isinstance(board, np.ndarray)
         assert board.shape == (6, 8, 8)
 
     def test_uci_to_coordinates(self):
         move = chess.Move.from_uci("e2e4")
         coordinates = uci_to_coordinates(move)
+
         assert isinstance(coordinates, tuple)
         assert len(coordinates) == 2
         assert len(coordinates[0]) == 2
@@ -49,6 +57,7 @@ class DataUtilsTestCase(unittest.TestCase):
         board = chess.Board()
         moves = list(board.legal_moves)
         tensor = moves_to_tensor(moves)
+
         assert isinstance(tensor, np.ndarray)
         assert tensor.shape == (64, 64)
         assert tensor.sum() == len(moves)
@@ -57,6 +66,7 @@ class DataUtilsTestCase(unittest.TestCase):
     def test_board_to_tensor(self):
         board = chess.Board()
         tensor = board_to_tensor(board)
+
         assert isinstance(tensor, np.ndarray)
         assert tensor.shape == (12, 8, 8)
         assert tensor.sum() == 32
@@ -65,6 +75,27 @@ class DataUtilsTestCase(unittest.TestCase):
     def test_result_to_tensor(self):
         result = "1-0"
         tensor = result_to_tensor(result)
+
         assert isinstance(tensor, np.ndarray)
         assert tensor.shape == (1,)
         assert tensor.dtype == np.int8
+
+
+    def test_read_boards_from_pgn(self):
+        boards = read_boards_from_pgn(pgn_file=self.pgn_path)
+
+        assert isinstance(boards, list)
+        assert all(isinstance(board, chess.Board) for board in boards)
+        assert len(boards) == sum(self.len_moves_najdorf)
+        assert all(board.is_valid() for board in boards)
+
+        boards = read_boards_from_pgn(pgn_file=self.pgn_path,
+                                      start_move=10,
+                                      end_move=15)
+
+        assert len(boards) == sum([x - 25 for x in self.len_moves_najdorf])
+
+        boards = read_boards_from_pgn(pgn_file=self.pgn_path,
+                                      start_move=35,
+                                      end_move=35)
+        assert len(boards) == 20
