e6c8a57591d889547122d54db4cb947adcd31c6d
Antoine Déchappe <dechantoine@gmail.com>
2024-07-26 16:28:40 +0200

    test: test parallel play

--- test/test_games.py
+++ test/test_games.py
@@ -20,6 +20,7 @@ class MockModel(torch.nn.Module):
         x = self.flatten(x)
         return self.linear(x)
 
+
 class GameTestCase(unittest.TestCase):
     def setUp(self):
         self.fen = "rnbqkbnr/ppp1pppp/8/3p4/2PP4/8/PP2PPPP/RNBQKBNR b KQkq - 0 2"
@@ -72,11 +73,12 @@ class GameTestCase(unittest.TestCase):
         dl_agent = DLAgent(model=self.model, is_white=False)
         game = Game(player_1=stockfish_agent, player_2=dl_agent, board=None)
 
-        winner, termination, n_moves = game.play()
+        winner, termination, n_moves, move_stack = game.play()
 
         self.assertIsInstance(winner, BaseAgent)
         self.assertIsInstance(termination, chess.Termination)
         self.assertIsInstance(n_moves, int)
+        self.assertIsInstance(move_stack, list)
 
 
 class MatchTestCase(unittest.TestCase):
@@ -101,6 +103,16 @@ class MatchTestCase(unittest.TestCase):
     def test_play(self):
         match = Match(player_1=self.stockfish_agent, player_2=self.dl_agent, n_games=5)
         match.play()
+
+        self.assertEqual(len(match.results), 5)
+        assert all(isinstance(match.results[i][0], BaseAgent) for i in range(5))
+        assert all(isinstance(match.results[i][1], chess.Termination) for i in range(5))
+        assert all(isinstance(match.results[i][2], int) for i in range(5))
+        assert all(isinstance(match.results[i][3], list) for i in range(5))
+
+    def test_parallel_play(self):
+        match = Match(player_1=self.stockfish_agent, player_2=self.dl_agent, n_games=5)
+        match.parallel_play()
 
         self.assertEqual(len(match.results), 5)
         assert all(isinstance(match.results[i][0], BaseAgent) for i in range(5))
