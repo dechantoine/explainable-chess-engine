20e5ea2cafff386d0823e6b79b7cc623caaada8f
Antoine Déchappe <dechantoine@gmail.com>
2024-09-13 17:01:05 +0200

    feat: get model

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,11 +2,14 @@ import torch
 
 
 def get_model(model_name: str) -> torch.nn.Module:
-    if model_name == "simple_feed_forward":
-        from .simple_feed_forward import SimpleFF
-        return SimpleFF()
-    elif model_name == "multi_input_feedforward":
+    if model_name == "multi_input_feedforward":
         from .multi_input_feedforward import MultiInputFF
         return MultiInputFF()
+    elif model_name == "multi_input_conv":
+        from .multi_input_conv import MultiInputConv
+        return MultiInputConv()
+    elif model_name == "multi_input_attention":
+        from .multi_input_attention import AttentionModel
+        return AttentionModel()
     else:
         raise ValueError(f"Unsupported model: {model_name}")
