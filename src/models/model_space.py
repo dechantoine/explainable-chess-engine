113aeaaed6b3beb99df80f10e7b0bb4eb101e777
Antoine Déchappe <dechantoine@gmail.com>
2024-10-08 11:11:17 +0200

    feat: new model for HF space

--- src/models/model_space.py
+++ src/models/model_space.py
@@ -9,26 +9,14 @@ class MultiInputConv(torch.nn.Module):
     def __init__(self):
         super().__init__()
         self.flatten = torch.nn.Flatten()
-        self.conv_long = torch.nn.Sequential(
-            torch.nn.Conv2d(in_channels=12, out_channels=16, kernel_size=15, padding=7, stride=2),
-            torch.nn.LeakyReLU(),
-            torch.nn.Conv2d(in_channels=16, out_channels=4, kernel_size=7, padding=3, stride=2),
-            torch.nn.LeakyReLU(),
-        )
-        self.conv_middle = torch.nn.Sequential(
-            torch.nn.Conv2d(in_channels=12, out_channels=16, kernel_size=9, padding=4, stride=2),
-            torch.nn.LeakyReLU(),
-            torch.nn.Conv2d(in_channels=16, out_channels=4, kernel_size=7, padding=3, stride=2),
-            torch.nn.LeakyReLU(),
-        )
-        self.conv_short = torch.nn.Sequential(
-            torch.nn.Conv2d(in_channels=12, out_channels=16, kernel_size=5, padding=2, stride=2),
-            torch.nn.LeakyReLU(),
-            torch.nn.Conv2d(in_channels=16, out_channels=4, kernel_size=7, padding=3, stride=2),
+        self.conv = torch.nn.Sequential(
+            torch.nn.Conv2d(in_channels=12, out_channels=64, kernel_size=15, padding=7, stride=1),
             torch.nn.LeakyReLU(),
         )
         self.linear_relu_stack = torch.nn.Sequential(
-            torch.nn.Linear(in_features=(4 * 2 * 2) + (4 * 2 * 2) + (4 * 2 * 2) + 1 + 4, out_features=16),
+            torch.nn.Linear(in_features=(64 * 8 * 8) + 1 + 4, out_features=64),
+            torch.nn.LeakyReLU(),
+            torch.nn.Linear(in_features=64, out_features=16),
             torch.nn.LeakyReLU(),
             torch.nn.Linear(in_features=16, out_features=1),
         )
@@ -41,16 +29,10 @@ class MultiInputConv(torch.nn.Module):
         color = color.float()
         castling = castling.float()
 
-        long = self.conv_long(board)
-        long = self.flatten(long)
+        conv = self.conv(board)
+        conv = self.flatten(conv)
 
-        middle = self.conv_middle(board)
-        middle = self.flatten(middle)
-
-        short = self.conv_short(board)
-        short = self.flatten(short)
-
-        x = torch.cat((long, middle, short, color, castling), dim=1)
+        x = torch.cat((conv, color, castling), dim=1)
 
         score = self.linear_relu_stack(x)
         return score
